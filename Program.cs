d0fc37b635f796466a96d0b22ee143a347d19502
Vincent Borry <vincent.borry@nobeltec.com>
2023-05-26 10:43:48 -0700

    messages

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Running...");
+            try
+            {
+                Test().Wait();
+            }
+            catch {}
+            Console.WriteLine("Done");
+        }
+
+        private static async Task Test()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            int timeout = 3000;
+            cts.CancelAfter(timeout);
+            while (await DoSomething(cts.Token))
+            {
+                timeout = timeout / 2;
+                cts.CancelAfter(timeout);
+            }
+        }
+
+        private static async Task<bool> DoSomething(CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"{DateTime.Now} ***Doing Something***");
+            await Task.Delay(1000, cancellationToken);
+            return true;
         }
     }
 }
